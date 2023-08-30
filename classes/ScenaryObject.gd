1347219d0a442760415d0b81a534ad166c1bd12b
Santi FC <oberkom@gmail.com>
2023-08-30 19:55:29 +0200

    Added fog particles when getting fire

--- classes/ScenaryObject.gd
+++ classes/ScenaryObject.gd
@@ -2,6 +2,7 @@ class_name ScenaryObject extends StaticBody3D
 
 @export var health = 3  # default health
 
+var fog_particles   = load("res://particles/fog.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +16,10 @@ func _process(delta):
 
 func get_shoot() :
 	health = health - 1
+	var fog = fog_particles.instantiate()
+	fog.position = get_node( 'CollisionShape3D' ).position
+	add_child( fog )
+		
 	if ( health == 0 ) :
 		hide()
 		#remove_child(self)
